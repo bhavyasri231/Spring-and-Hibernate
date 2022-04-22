75c3152c2be922764db2fed29de4de3763a993c8
Bhavyasri.Penta <bhavyasri.penta@zemosolabs.com>
2022-04-22 10:20:56 +0530

    After Finally

---
+++ src/main/java/com/springdemo/aop/AfterThrowingDemoApp.java
@@ -0,0 +1,40 @@
+package com.springdemo.aop;
+
+
+import com.springdemo.aop.Account;
+import com.springdemo.aop.DemoConfig;
+import com.springdemo.aop.dao.AccountDAO;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+
+public class AfterThrowingDemoApp {
+
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(DemoConfig.class);
+
+        AccountDAO theAccountDAO = context.getBean("accountDAO",AccountDAO.class);
+
+        List<Account> theAccounts = null;
+        try
+        {
+            boolean tripWire = true;
+            theAccounts = theAccountDAO.findAccounts(tripWire);
+        }
+        catch (Exception exc)
+        {
+            System.out.println("\n\nMain program... caught exception: "+exc);
+        }
+
+        System.out.println("\n\nMain program: AfterThrowingDemoApp");
+        System.out.println("-----");
+
+        System.out.println(theAccounts);
+         
+
+        context.close();
+    }
+}
