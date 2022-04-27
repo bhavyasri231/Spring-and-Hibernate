5395a664bc6dd0810ecfe9451abd79a3354437ff
Bhavyasri.Penta <bhavyasri.penta@zemosolabs.com>
2022-04-27 11:16:00 +0530

    Spring Rest

---
+++ src/main/java/com/springdemo/rest/StudentRestController.java
@@ -0,0 +1,42 @@
+package com.springdemo.rest;
+
+import com.springdemo.entity.Student;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    private List<Student> theStudents;
+
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
+
+        theStudents.add(new Student("bhavya", "sri"));
+        theStudents.add(new Student("Poornima", "Patel"));
+        theStudents.add(new Student("Mary", "smith"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return theStudents;
+
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        if ((studentId >= theStudents.size()) || (studentId < 0)) {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+        return theStudents.get(studentId);
+    }
+}
+
+
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<html>
+    <body>
+        <h3> Spring Rest Demo </h3>
+        <hr>
+
+    </body>
+</html>
